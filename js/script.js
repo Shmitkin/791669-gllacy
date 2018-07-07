cfd6b055535e5cd6a3ea4f295fe1c5758331b9df
Shmitkin <mitkinoleg@Gmail.com>
2018-07-07 16:53:53 +0300

    Внес все правки

--- js/script.js
+++ js/script.js
@@ -1,19 +1,16 @@
+var link = document.querySelector(".feedback-button");
+var popup = document.querySelector(".feedback-popup");
+var popupbg = document.querySelector(".popup-bg");
+var close = popup.querySelector(".button-popup-close");
 
-	var link = document.querySelector(".feedback-button");
+link.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.add("popup-show");
+  popupbg.classList.add("popup-bg-active");
+});
 
-	var popup = document.querySelector(".feedback-popup");
-	var popupbg = document.querySelector(".popup-bg");
-	 var close = popup.querySelector(".button-popup-close");
-
-	link.addEventListener("click", function (evt) {
-		evt.preventDefault();
-		popup.classList.add("popup-show");
-		popupbg.classList.add("popup-bg-active");
-
-  });
-
-	close.addEventListener("click", function (evt) {
-    evt.preventDefault();
-    popup.classList.remove("popup-show");
-    popupbg.classList.remove("popup-bg-active");
-  });
+close.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.remove("popup-show");
+  popupbg.classList.remove("popup-bg-active");
+});
