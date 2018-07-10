1d285e289f433fa20bf0d52a5df2ad5ad40d2017
Shmitkin <mitkinoleg@Gmail.com>
2018-07-10 13:12:14 +0300

    Д17

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@ var link = document.querySelector(".feedback-button");
 var popup = document.querySelector(".feedback-popup");
 var popupbg = document.querySelector(".popup-bg");
 var close = popup.querySelector(".button-popup-close");
+var form = popup.querySelector(".feedback-form");
+var email = popup.querySelector("[name=feedback-email]");
 
 link.addEventListener("click", function (evt) {
   evt.preventDefault();
@@ -13,4 +15,25 @@ close.addEventListener("click", function (evt) {
   evt.preventDefault();
   popup.classList.remove("popup-show");
   popupbg.classList.remove("popup-bg-active");
+  popup.classList.remove("popup-error");
 });
+
+ form.addEventListener("submit", function (evt) {
+    if (!email.value) {
+  evt.preventDefault();
+  popup.classList.remove("popup-error");
+  popup.offsetWidth = popup.offsetWidth;
+  popup.classList.add("popup-error");;
+  }
+});
+
+  window.addEventListener("keydown", function (evt) {
+    if (evt.keyCode === 27) {
+      evt.preventDefault();
+      if (popup.classList.contains("popup-show")) {
+        popup.classList.remove("popup-show");
+        popupbg.classList.remove("popup-bg-active");
+        popup.classList.remove("popup-error");
+      }
+    }
+  });
